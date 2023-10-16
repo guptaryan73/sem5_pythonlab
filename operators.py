acece257013b64bca6198b741afae1d8647997af
guptaryan73 <guptaryan73@gmail.com>
2023-10-16 13:28:25 +0530

    added till experiment 6

---
+++ operators.py
@@ -0,0 +1,10 @@
+""" types of operators in python 
+
+    1. arithmetic operators
+    2. logical operators
+    3. bitwise operators
+    4. special operators //  anmd ** 
+        // --> floor division
+        return the integer part of the decimal part
+
+"""
