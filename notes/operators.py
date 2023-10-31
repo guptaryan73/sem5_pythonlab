25ef61247f63cc2eb5a07e3fac493ce2c8a0afc4
guptaryan73 <guptaryan73@gmail.com>
2023-10-31 15:03:20 +0530

    changes made

---
+++ notes/operators.py
@@ -0,0 +1,22 @@
+""" 
+types of operators in python 
+
+    1. arithmetic operators
+    2. logical operators
+    3. bitwise operators
+    4. special operators //  anmd ** 
+        // --> floor division
+        return the integer part of the decimal part
+    5. relational operatos
+        python allows chaining of multiple chaining opearators
+    6. identity operators
+        is and is not
+    7. membership operators
+    
+        
+
+    DOUBLE ROLE OF THE OPERATOR +
+
+
+
+"""
